d4bc6461801c0d4ea9dbe97a94aa05f6649788e9
Vishank Bhatia <vishank94@gmail.com>
2018-01-24 00:17:30 -0800

    Create SortByNumberofRatings.py

---
+++ SortByNumberofRatings.py
@@ -0,0 +1,33 @@
+#movieID - Sorted by number of ratings for that movie
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class RatingsBreakdown(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings)
+        ]
+
+    def mapper_get_ratings(self, _, line):
+        #(userID, movieID, rating, timestamp) = line.split('\t')
+        #i=0
+        #while True:
+        alist = line.split('\t')
+        #print(alist)
+        #i = i+1
+        int ratingCount = 0
+        userID = alist[0]
+        movieID = alist[1]
+        rating = alist[2]
+        timestamp = alist[3]
+        #ratingCount = ratingCount + 1
+        yield movieID, 1
+
+    def reducer_count_ratings(self, key, values):
+        yield sum(values), movieID
+        #yield key, sum(values)
+
+if __name__ == '__main__':
+    RatingsBreakdown.run()
