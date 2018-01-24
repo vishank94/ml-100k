2c4d43d7ee6e88795e1246c814af55f466ff616c
Vishank Bhatia <vishank94@gmail.com>
2018-01-23 20:51:04 -0800

    Update RatingsBreakdownList.py

--- RatingsBreakdownList.py
+++ RatingsBreakdownList.py
@@ -11,15 +11,15 @@ class RatingsBreakdown(MRJob):
     def mapper_get_ratings(self, _, line):
         #(userID, movieID, rating, timestamp) = line.split('\t')
         #i=0
-        while True:
-            alist = line.split('\t')
-            print(alist)
-            #i = i+1
-            userID = alist[0]
-            movieID = alist[1]
-            rating = alist[2]
-            timestamp = alist[3]
-            yield rating, 1
+        #while True:
+        alist = line.split('\t')
+        #print(alist)
+        #i = i+1
+        userID = alist[0]
+        movieID = alist[1]
+        rating = alist[2]
+        timestamp = alist[3]
+        yield rating, 1
 
     def reducer_count_ratings(self, key, values):
         yield key, sum(values)
