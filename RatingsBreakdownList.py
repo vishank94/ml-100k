f29f4ab81327e73daa1d986e7e29c264512f4f9c
Vishank Bhatia <vishank94@gmail.com>
2018-01-23 19:54:58 -0800

    Add files via upload

---
+++ RatingsBreakdownList.py
@@ -0,0 +1,28 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+#class RatingsBreakdown(MRJob):
+#    def steps(self):
+#        return [
+#            MRStep(mapper=self.mapper_get_ratings,
+#                   reducer=self.reducer_count_ratings)
+#        ]
+class hello():
+    def mapper_get_ratings(self, _, line):
+        #(userID, movieID, rating, timestamp) = line.split('\t')
+        while True:
+			alist = ("My name is Vishank").split('\t')
+
+			if len(alist) is 4:
+			break
+		userID = alist[0]
+		movieID = alist[1]
+		rating = = alist[2]
+		timestamp = alist[3]
+		yield rating, 1
+
+    def reducer_count_ratings(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    RatingsBreakdown.run()
