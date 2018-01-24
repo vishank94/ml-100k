a6124e55a06f4c7a747e5aa16fab2eecaf96d413
Vishank Bhatia <vishank94@gmail.com>
2018-01-23 20:20:06 -0800

    Update RatingsBreakdownList.py

--- RatingsBreakdownList.py
+++ RatingsBreakdownList.py
@@ -1,25 +1,25 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-#class RatingsBreakdown(MRJob):
-#    def steps(self):
-#        return [
-#            MRStep(mapper=self.mapper_get_ratings,
-#                   reducer=self.reducer_count_ratings)
-#        ]
-class hello():
+class RatingsBreakdown(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings)
+        ]
+
     def mapper_get_ratings(self, _, line):
         #(userID, movieID, rating, timestamp) = line.split('\t')
         while True:
 			alist = line.split('\t')
 
-			if len(alist) is 4:
-			break
-		userID = alist[0]
-		movieID = alist[1]
-		rating = = alist[2]
-		timestamp = alist[3]
-		yield rating, 1
+		#	if len(alist) is 4:
+		#		break
+			userID = alist[0]
+			movieID = alist[1]
+			rating = alist[2]
+			timestamp = alist[3]
+			yield rating, 1
 
     def reducer_count_ratings(self, key, values):
         yield key, sum(values)
